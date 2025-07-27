78d8cadc3cc3f0ed8d53637afcd5513f0b10d010
Abigail Bales <138115235+Abagels96@users.noreply.github.com>
2025-07-26 21:31:17 -0500

    adding fixes?

--- src/main/java/com/coderscampus/assignment13/service/AddressService.java
+++ src/main/java/com/coderscampus/assignment13/service/AddressService.java
@@ -1,21 +1,18 @@
 package com.coderscampus.assignment13.service;
 
-
 import org.springframework.stereotype.Service;
 
 import com.coderscampus.assignment13.domain.Address;
 import com.coderscampus.assignment13.domain.User;
 import com.coderscampus.assignment13.repository.AddressRepository;
+
 @Service
 public class AddressService {
-	
+
 	AddressRepository addressRepo;
-	
+
 	public Address saveAddress(Address address) {
 		return addressRepo.save(address);
 	}
-	
-	
-	
-	
+
 }
--- src/main/java/com/coderscampus/assignment13/web/UserController.java
+++ src/main/java/com/coderscampus/assignment13/web/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -19,70 +20,67 @@ import com.coderscampus.assignment13.service.UserService;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
 	@Autowired
 	private AddressService addressService;
-	
-	
-	
+
 	@GetMapping("/register")
-	
-	public String getCreateUser (ModelMap model) {
-		
+
+	public String getCreateUser(ModelMap model) {
+
 		model.put("user", new User());
 		model.put("address", new Address());
 //		model.put("account", user.getAccounts().get(0).toString());
-		
+
 		return "register";
 	}
-	
+
 	@PostMapping("/register")
-	public String postCreateUser (User user) {
-		
+	public String postCreateUser(@ModelAttribute User user) {
+
 		System.out.println(user);
-		
-		
+
 		// addAccountToUser- rename at some point
-		if( user.getUserId()==null) {
-		userService.addAddress(user);
-	 addressService.saveAddress(user.getAddress()); 
-		userService.saveUser(user);
+		if (user.getUserId() == null) {
+//		userService.addAddress(user);
+			addressService.saveAddress(user.getAddress());
+			userService.saveUser(user);
 		}
 //		userService.addAccount(user, user.getAccounts().add(0, null));
 		return "redirect:/register";
 	}
-	
+
 	@GetMapping("/users")
-	public String getAllUsers (ModelMap model) {
+	public String getAllUsers(ModelMap model) {
 		Set<User> users = userService.findAll();
-		
+
 		model.put("users", users);
 		if (users.size() == 1) {
 			model.put("user", users.iterator().next());
 		}
-		
+
 		return "users";
 	}
-	
+
 	@GetMapping("/users/{userId}")
-	public String getOneUser (ModelMap model, @PathVariable Long userId) {
+	public String getOneUser(ModelMap model, @PathVariable Long userId) {
 		User user = userService.findById(userId);
-	 		model.put("users", Arrays.asList(user));
+		model.put("users", Arrays.asList(user));
 		model.put("user", user);
 		return "userId";
 	}
-	
+
 	@PostMapping("/users/{userId}")
-	public String postOneUser (User user) {
+	public String postOneUser(User user) {
 		userService.saveUser(user);
-		
-		return "redirect:/users/"+user.getUserId();
+
+		return "redirect:/users/" + user.getUserId();
 	}
-	
+
 	@PostMapping("/users/{userId}/delete")
-	public String deleteOneUser (@PathVariable Long userId) {
+	public String deleteOneUser(@PathVariable Long userId) {
 		userService.delete(userId);
 		return "redirect:/users";
 	}
